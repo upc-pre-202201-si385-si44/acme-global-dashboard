84d3daa3f069bd9f112a55cc9dae8300aaf3c768
Angel Velasquez <pcisavel@upc.edu.pe>
2022-06-17 16:55:55 -0500

    feat(dashboard): Added Pie Graphics drawing.

--- public/assets/scripts/charts.js
+++ public/assets/scripts/charts.js
@@ -7,7 +7,29 @@ function drawCharts() {
 }
 
 function drawPieChart() {
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Project Type');
+    data.addColumn('number', 'Thousands of Requests');
+    data.addRows([
+        ['Automation', 3],
+        ['Event Processing', 1],
+        ['Gamification', 1],
+        ['Geofencing', 1],
+        ['Internet of Things', 2]
+    ]);
 
+    const options = {
+        'title': 'Technology Projects by Type',
+        'width': 500,
+        'height': 300
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('pie-placeholder'));
+    chart.draw(data, options);
+
+    $(window).resize(function () {
+        chart.draw(data, options);
+    })
 }
 
 function drawAxisTickColors() {
